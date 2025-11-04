371870a4ed41b1c3a5bd27275c6c9c660b68ad3c
Sanjay R <sanpro2004.jay@gmail.com>
2025-11-04 16:50:07 +0530

    change vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
 /**
-* @type {import('vite').UserConfig}
-*/
+ * @type {import('vite').UserConfig}
+ */
 export default {
-  // Set the base directory for GitHub pages
-  base: process.env.NODE_ENV === 'production' ? '/threejs-image-gallery/' : '/',
+  // Set base to root for Vercel deployment
+  base: './',
+
   build: {
     outDir: './dist',
     sourcemap: true,
   },
+
   publicDir: './public',
 }
